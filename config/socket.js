3aeae38e3ce7a9fc2a8bb9ac2fc225502213a57b
AmmarOsamaAli <ammarosama080@icloud.com>
2026-08-12 10:24:56 +0300

    Fixed socket.js to allow users to see other participants in the waiting room

--- config/socket.js
+++ config/socket.js
@@ -67,7 +67,22 @@ function initializeSocket(server) {
 
                 socket.join(foundGame._id.toString())
 
+                const participants = await Participant.find({
+                    game: foundGame._id
+                }).populate("user", "username")
+
+                const lobbyPlayers = participants.map((participant) => ({
+                    _id: participant.user._id,
+                    username: participant.user.username
+                }))
+
                 console.log(`User ${socket.user.username} joined game room ${foundGame._id}`)
+
+                io.to(foundGame._id.toString()).emit(
+                    "lobbyPlayers",
+                    lobbyPlayers
+                )
+
 
                 socket.emit("gameRoomJoined", { gameId: foundGame._id })
 
